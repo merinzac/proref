ac2f74d29dd20462309da48a6db8d43603ab7063
merinzac <70364261+merinzac@users.noreply.github.com>
2021-02-06 05:42:26 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# proref
+# afa156b839e1b81fbfa9bf61b99ed6eb
---
+++ sketch.js
@@ -0,0 +1,12 @@
+var Dogsprite, foodS, happyDog,database,dog,foodStock;
+function preload(){
+  //load images here
+  dog = loadImage("Images/Dog.png");
+  happyDog=loadImage("Images/happy dog.png");
+}
+
+function setup() {
+}
+function draw(){
+  
+}
